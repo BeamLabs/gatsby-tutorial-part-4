45eb4a5a050fd71b72caafee739f13ac4191086a
Andre Giron <agiron123@gmail.com>
2020-08-06 04:33:52 +0000

    Testing out codespaces.

---
+++ src/pages/hello-codespaces.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Hello from Github CodeSpaces!</h1>
+    <p>
+      testing out CodeSpaces here.
+    </p>
+  </Layout>
+)
